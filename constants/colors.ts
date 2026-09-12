9a25f40c1fea4d2bf61359d7029077e7bc91c130
Noely-Jed-E-Arcay <drixgael@gmail.com>
2026-09-12 11:11:37 +0800

    EAS commit

---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: '#a09041',
+  background: '#35383b',
+  card: '#80796d',
+  textPrimary: '#cecece',
+  textSecondary: '#dfd3bb',
+  textOnPrimary: '#533711',
+  surface: '#635b5b',
+  border: '#E0E8F0',
+  shadow: '#0D47A1',
+} as const;
